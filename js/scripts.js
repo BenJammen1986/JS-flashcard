f5f5f7d6c34073420a8fd0f8b447050bb4608bd8
Ben Schenkenberger and Janek Brandt <ben@>
2017-02-14 13:31:33 -0800

    modify jquery to hide content on second click of item

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(".clickableList li").click(function() {
     var itemClass = $(this).attr("class");
-    $(".content ."+itemClass).fadeToggle();
+    $(".content p:not(."+itemClass+")").slideUp();
+    $(".content ."+itemClass).slideToggle();
   });
 });
